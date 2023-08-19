0b9ee17d6d816ed31b3a5b036b95a5bed94242ee
ykv749 <ykv749@gmail.com>
2023-08-20 00:28:23 +0530

    UI Fixes

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import image from '../assets/Image.png'
+import image from '../assets/Image.png';
+import Desktopimage from '../assets/DesktopImage.png';
 
 const Intro = () => {
   return (
     <section id='intro' className='relative flex items-center justify-between h-[100dvh] gap-5 mobile:justify-center mobile:h-[auto]'>
-        <div className='introContent z-10 mobile:absolute mobile:bottom-[10%]'>
+        <div className='introContent z-10 mobile:absolute mobile:bottom-[40%]'>
             <span className='text-[25px] mobile:text-[20px]'>Hello,</span><br/>
             <span className='text-[3.5rem] font-semibold mobile:text-[30px]'>I'm <span className='text-[red]'> Yash</span><br/>Frontend Developer</span>
             <p className="text-[14px] max-w-[50%] mobile:max-w-[100%]  mobile:text-[10px]">Experienced frontend developer with a keen interest in problem-solving and a solid foundation in Data Structures and Algorithms (DSA). Skilled in developing responsive and intuitive user interfaces using HTML, CSS, JavaScript, and ReactJS. Passionate about building applications that leave a positive impact on users' lives. Always eager to learn and explore new technologies.</p>
             {/* <button><img src={} /></button> */}
         </div>
         <div className='absolute right-0 z-[-1] mobile:relative'>
-            <img className='h-[65rem] mobile:h-[fit-content]' src={image} alt='myPhoto' />    
+            <img className='mobile:h-[fit-content] desktop:hidden' src={image} alt='myPhoto' />    
+            <img className='h-[65rem] mobile:hidden' src={Desktopimage} alt='myPhoto' />    
         </div>
     </section>
   )
