f6590abbfb5015e2b5d2c9ee55983bca393b3d30
ykv749 <ykv749@gmail.com>
2023-08-20 10:57:35 +0530

    UI Fixes

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import github from '../assets/github.png';
 
 const Footer = () => {
   return (
-    <div className='flex items-center mobile:flex-col-reverse mobile:justify-center justify-between desktop:mt-[10rem]'>
+    <div className='flex items-center mobile:flex-col-reverse mobile:justify-center justify-between desktop:mt-[10rem] mobile:mt-[2rem]'>
       <p className='mobile:text-[14px]'>Made with ❤️ by Yash Kumar Verma (ykv749)</p>
       <ul className='flex gap-1'>
         <li><a href='https://www.linkedin.com/in/ykv749/' rel='noreferrer' target="_blank"><img src={linkedin} className='w-[50px]' alt='linkedin'/></a></li>
