1edc4f3532fed541023b2b39194bd0f3a4707ec5
ykv749 <ykv749@outlook.com>
2025-11-10 21:07:09 +0530

    Image Updated

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import image from '../assets/Image.png';
-import Desktopimage from '../assets/DesktopImage.png';
+import image from '../assets/Image.jpg';
+import Desktopimage from '../assets/DesktopImage.jpg';
 
 const Intro = () => {
   return (
     <section id='intro' className='relative flex items-center justify-between h-[100dvh] gap-5 mobile:justify-center mobile:h-[auto]'>
         <div className='introContent z-10 mobile:absolute mobile:bottom-[30%]'>
             <span className='text-[25px] mobile:text-[20px]'>Hello,</span><br/>
-            <span className='text-[3.5rem] font-semibold mobile:text-[30px]'>I'm <span className='text-[red]'> Yash</span><br/>Frontend Developer</span>
-            <p className="text-[14px] max-w-[50%] mobile:max-w-[100%]  mobile:text-[10px]">Experienced frontend developer with a keen interest in problem-solving and a solid foundation in Data Structures and Algorithms (DSA). Skilled in developing responsive and intuitive user interfaces using HTML, CSS, JavaScript, and ReactJS. Passionate about building applications that leave a positive impact on users' lives. Always eager to learn and explore new technologies.</p>
+            <span className='text-[3.5rem] font-semibold mobile:text-[30px]'>I'm <span className='text-[red]'> Yash</span><br/>Software Developer</span>
+            <p className="text-[14px] max-w-[50%] mobile:max-w-[100%]  mobile:text-[10px]">
+              Experienced backend engineer with a passion for complex problem-solving and a solid foundation in Data Structures and Algorithms (DSA). 
+              Skilled in designing and implementing scalable backend systems, microservices, and clean APIs. 
+              Proficient in Golang Backend Frameworks. Passionate about engineering high-performance applications that leave a positive impact on users' lives. 
+              Always eager to learn and explore new technologies.
+            </p>
             {/* <button><img src={} /></button> */}
         </div>
         <div className='absolute right-0 z-[-1] mobile:relative'>
